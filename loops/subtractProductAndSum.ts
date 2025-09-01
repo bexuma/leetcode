fa5c4e2cd2b4abf0dbe4976906e85da305c1dddc
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-09-01 19:21:49 +0500

    refactor(subtractProductAndSum): omit copying input number

--- loops/subtractProductAndSum.ts
+++ loops/subtractProductAndSum.ts
@@ -1,20 +1,17 @@
 // 1281. Subtract the Product and Sum of Digits of an Integer
 // https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 
-// Sol 1
 function subtractProductAndSum(n: number): number {
     let product = 1;
     let sum = 0;
 
-    let tmp = n;
-
-    while (tmp >= 10) {
-        product *= tmp % 10;
-        sum += tmp % 10;
-        tmp = Math.floor(tmp / 10);
+    while (n >= 10) {
+        product *= n % 10;
+        sum += n % 10;
+        n = Math.floor(n / 10);
     };
-    product *= tmp;
-    sum += tmp;
+    product *= n;
+    sum += n;
 
     return product - sum;
 };
