964c5f10ed21497addd43c64fd51093fc06f1cbc
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-09-01 18:30:20 +0500

    125. Valid Palindrome
    https://leetcode.com/problems/valid-palindrome/description

---
+++ 123_Valid_Palindrome.ts
@@ -0,0 +1,16 @@
+function isPalindrome(s: string): boolean {
+    if (s.length === 0) return true;
+
+    const isAlphanumeric = (c: string): boolean => {
+        return (c >= "0" && c <= "9") || (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
+    };
+
+    for (let l = 0, r = s.length - 1; l < r; l++, r--) {
+        while (!isAlphanumeric(s[l]) && l <= r) l++;
+        while (!isAlphanumeric(s[r]) && r >= l) r--;        
+        if (!isAlphanumeric(s[r])) return true;
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+    };
+
+    return true;
+};
