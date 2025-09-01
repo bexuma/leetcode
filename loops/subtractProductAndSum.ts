d9d6aa815de45dc5415337099da1cebd1e6482e7
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-09-01 19:25:03 +0500

    refactor(subtractProductAndSum): solidify the loop

--- loops/subtractProductAndSum.ts
+++ loops/subtractProductAndSum.ts
@@ -5,13 +5,11 @@ function subtractProductAndSum(n: number): number {
     let product = 1;
     let sum = 0;
 
-    while (n >= 10) {
+    while (n > 0) {
         product *= n % 10;
         sum += n % 10;
         n = Math.floor(n / 10);
     };
-    product *= n;
-    sum += n;
 
     return product - sum;
 };
