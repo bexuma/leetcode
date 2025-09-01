27bf3b4fed2f3ca3ebf09a28aeaa6808956a4b0c
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-09-01 18:32:58 +0500

    344. Reverse String

---
+++ 344_Reverse_String.ts
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/reverse-string/
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let c: string = '';
+
+    for (let left = 0, right = s.length - 1; left < right; left++, right--) {
+        // [s[left], s[right]] = [s[right], s[left]];
+        c = s[left];
+        s[left] = s[right];
+        s[right] = c;
+    };
+};
